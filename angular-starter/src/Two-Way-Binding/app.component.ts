ee692db2fd21dde92a27b05b9ba7ae910608bb74
rachitgupta98 <rachit371@gmail.com>
2020-12-13 15:41:02 +0530

    changes

--- angular-starter/src/Two-Way-Binding/app.component.ts
+++ angular-starter/src/Two-Way-Binding/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  textIn:string="rahit";
+  textIn:string="rachit";
   getEnter(){
     console.log(this.textIn)
   }
